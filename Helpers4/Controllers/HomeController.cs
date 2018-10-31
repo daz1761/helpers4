0b1116e184d9bca2a8d7616c32a187529092a479
daz1761 <dazgti@btinternet.com>
2018-10-31 20:15:44 +0000

    Project commit

---
+++ Helpers4/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Helpers4.Models;
+
+namespace Helpers4.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Details(int id)
+        {
+            SampleDBContext db = new SampleDBContext();
+            Employee employee = db.Employees.Single(x => x.Id == id);
+
+            return View(employee);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ Helpers4/Models/_Employee.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Helpers4.Models
+{
+    [MetadataType(typeof(EmployeeMetadata))]
+    public partial class Employee
+    {
+        // I think we use these in Umbraco which explains things now with the Attributes overriding model properties
+    }
+
+    public class EmployeeMetadata
+    {
+        //[DisplayAttribute(Name = "Full Name")]
+        //[Display(Name = "Full Name")]
+        [DisplayName("Full Name")]
+        public string FullName { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:d}")]
+        public DateTime? HireDate { get; set; }
+
+        [DisplayFormat(NullDisplayText ="Gender Not Specified - maybe this is a transgender cunt")]
+        public string Gender { get; set; }
+
+        [ScaffoldColumn(false)]
+        public int? Id { get; set; }
+
+        [ScaffoldColumn(false)]
+        public int? Salary { get; set; }
+    }
+
+}
